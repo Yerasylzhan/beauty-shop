d6cf09781b08e8191f1bc424a24b7c07845d983f
Yerasylzhan <asikasik9000@gmail.com>
2023-12-31 20:06:11 +0600

    feat: signup logic fixed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs"
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
