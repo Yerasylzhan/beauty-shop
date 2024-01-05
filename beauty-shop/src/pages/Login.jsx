a2011f5d8e4c3157abe41582f304a7745143e4c2
Yerasylzhan <asikasik9000@gmail.com>
2024-01-06 04:11:56 +0600

    feat: added redux and persistance

--- beauty-shop/src/pages/Login.jsx
+++ beauty-shop/src/pages/Login.jsx
@@ -1,21 +1,25 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function Login() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {
@@ -26,16 +30,13 @@ export default function Login() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
-      navigate('/login');
+      dispatch(signInSuccess(data));
+      navigate('/main');
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
@@ -62,7 +63,7 @@ export default function Login() {
           disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          {loading ? 'Загрузка...' : 'Зарегистрироваться'}
+          {loading ? 'Загрузка...' : 'Авторизоваться'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
