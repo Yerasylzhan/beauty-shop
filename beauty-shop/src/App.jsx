c8244d58e899db2506a5dbf1756ff1f02eabc6a3
Yerasylzhan <asikasik9000@gmail.com>
2024-01-07 21:40:34 +0600

    feat: profile page photo on topbat and privacy

--- beauty-shop/src/App.jsx
+++ beauty-shop/src/App.jsx
@@ -1,11 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Register from "./pages/Register"
-import Logout from "./pages/Logout"
-import Login from "./pages/Login"
-import Main from "./pages/Main"
-import Topbar from "./components/Topbar"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+import Logout from "./pages/Logout";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import Topbar from "./components/Topbar";
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
   return <BrowserRouter>
@@ -13,7 +14,9 @@ export default function App() {
   <Routes>
     <Route path="/" element={<Main />} />
     <Route path="/about" element={<About />} />
-    <Route path="/profile" element={<Profile />} />
+    <Route element={<PrivateRoute />} >
+      <Route path="/profile" element={<Profile />} />
+    </Route>
     <Route path="/register" element={<Register />} />
     <Route path="/login" element={<Login />} />
     <Route path="/logout" element={<Logout />} />
--- beauty-shop/src/components/Topbar.jsx
+++ beauty-shop/src/components/Topbar.jsx
@@ -1,7 +1,11 @@
-import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import {FaSearch} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function Topbar() {
+const {currentUser} = useSelector((state) => state.user)
+//console.log(currentUser.avatar)
+
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -21,8 +25,11 @@ export default function Topbar() {
                 <Link to='/about'>
                     <li className='hidden sm:inline text-slate-700 hover-underline'> О нас</li>
                 </Link>
-                <Link to='/login'>
-                    <li className='sm:inline text-slate-700 hover-underline'>Войти</li>
+                <Link to='/profile'>
+                    {currentUser ? (
+                        <img className='rounded-full h-7 w-7 object-cover' src = {currentUser.avatar} alt='profile'/>
+                    ) : <li className='sm:inline text-slate-700 hover-underline'>Войти</li>
+                    }
                 </Link>
             </ul>
         </div>
